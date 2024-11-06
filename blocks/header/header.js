f98e2c06e621424f844c10955850a092e9b67c3a
svfranklindemo <166366015+svfranklindemo@users.noreply.github.com>
2024-11-06 12:21:12 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -57,7 +57,7 @@ function focusNavSection() {
  * @param {Boolean} expanded Whether the element should be expanded or collapsed
  */
 function toggleAllNavSections(sections, expanded = false) {
-  sections.querySelectorAll('.nav-sections .default-content-wrapper > ul > li').forEach((section) => {
+  sections?.querySelectorAll('.nav-sections .default-content-wrapper > ul > li').forEach((section) => {
     section.setAttribute('aria-expanded', expanded);
   });
 }
