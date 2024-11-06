81639aae4dee6974ed55f2f9b857b47c69222b1e
svfranklindemo <166366015+svfranklindemo@users.noreply.github.com>
2024-11-06 11:33:36 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -117,7 +117,7 @@ export default async function decorate(block) {
   block.textContent = '';
   const nav = document.createElement('nav');
   nav.id = 'nav';
-  while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
+  fragment && while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
 
   const classes = ['brand', 'sections', 'tools'];
   classes.forEach((c, i) => {
